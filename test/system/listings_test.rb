91f1f52b793500085d14ae44549010f3dbae4cbf
Adhiv Dhar <adhivd@gmail.com>
2018-04-21 12:15:09 -0700

    scaffolding yo

---
+++ test/system/listings_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class ListingsTest < ApplicationSystemTestCase
+  setup do
+    @listing = listings(:one)
+  end
+
+  test "visiting the index" do
+    visit listings_url
+    assert_selector "h1", text: "Listings"
+  end
+
+  test "creating a Listing" do
+    visit listings_url
+    click_on "New Listing"
+
+    click_on "Create Listing"
+
+    assert_text "Listing was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Listing" do
+    visit listings_url
+    click_on "Edit", match: :first
+
+    click_on "Update Listing"
+
+    assert_text "Listing was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Listing" do
+    visit listings_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Listing was successfully destroyed"
+  end
+end
