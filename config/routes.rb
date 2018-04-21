b8aed63b9452b78c724b879c72ae01cac20a6e9f
Adhiv Dhar <adhivd@gmail.com>
2018-04-21 12:36:57 -0700

    DO NOT USE

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :listings
   get 'home/show'
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
