e5db2cd3941a597f2d95b7ad5b8819d2036edbb0
Adhiv Dhar <adhivd@gmail.com>
2018-04-22 13:00:43 -0700

    favorites and listings

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -27,7 +27,7 @@ class FavoritesController < ApplicationController
   # POST /favorites.json
   def create
     @favorite = Favorite.new(favorite_params)
-			puts favorite_params
+			 puts favorite_params
     respond_to do |format|
       if @favorite.save
         format.html { redirect_to @favorite, notice: 'Favorite was successfully created.' }
