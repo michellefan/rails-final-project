9ed95ac65295db39e5421bb80b704c25cd1b06a6
Tina Ye <tinaye@berkeley.edu>
2018-04-22 13:41:17 -0700

    google maps gem markers to work with database info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,16 @@
 class HomeController < ApplicationController
-  
+
     @listings = Listing.all
     def show
     end
-    
+
     def index
         @listings = Listing.all
         @hash = Gmaps4rails.build_markers(@listings) do |listing, marker|
             marker.lat listing.latitude
             marker.lng listing.longitude
-            marker.infowindow listing.text
+            marker.json({name: listing.address})
+            marker.infowindow listing.address
         end
     end
 end
